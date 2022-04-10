1e12379e6eae5ecb6e654fedcc32795b96052615
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-04-10 14:13:29 +0300

    added model

---
+++ BoredParseJSON/BoredParseJSON/Models/Bored.swift
@@ -0,0 +1,15 @@
+//
+//  Bored.swift
+//  BoredParseJSON
+//
+//  Created by Евгения Шевцова on 10.04.2022.
+//
+
+import Foundation
+
+struct Bored: Decodable {
+    let activity: String?
+    let type: String?
+    let price: Double?
+    let accessibility: Double?
+}
---
+++ BoredParseJSON/BoredParseJSON/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  BoredParseJSON
+//
+//  Created by Евгения Шевцова on 10.04.2022.
+//
+
+import Foundation
